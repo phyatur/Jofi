c53dd9c6ca154a8b26d18c2fe3336d0522db80d2
phyatur <phyatur.official@gmail.com>
2022-09-28 15:17:39 +0000

    audio

--- index.js
+++ index.js
@@ -31,6 +31,25 @@ ELEMENT.forEach((element) => {
 });
 
 
+// MAPS
 
 
+var detailsBox = document.getElementById('details-box');
 
+document.addEventListener('mouseover', function (e) {
+  if (e.target.tagName == 'path') {
+    var content = e.target.dataset.name;
+    detailsBox.innerHTML = content;
+    detailsBox.style.opacity = "100%";
+  }
+  else {
+    detailsBox.style.opacity = "0%";
+  }
+});
+
+window.onmousemove = function (e) {
+  var x = e.clientX,
+      y = e.clientY;
+  detailsBox.style.top = (y + 20) + 'px';
+  detailsBox.style.left = (x) + 'px';
+};
